52c742855a482db138f85725fb9c1a4bc0c6e141
maymunadiop-crypto <maymunadiop@gmail.com>
2026-08-24 01:53:25 +0200

    add mobile navigation script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+</> Javascript
+const menuButton = document.querySelector(".menu-button");
+const mobileNav = document.querySelector(".mobile-nav");
+
+menuButton.addEventListener("click", () => {
+  mobileNav.classList.toggle("open");
+});
